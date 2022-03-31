c655c57d69292623c1d77b47dc2fbf7132f7bfcc
yonghk423 <yonghk423423@gmail.com>
2022-03-31 18:56:21 +0900

    start

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+// import './App.css';
+import './App.scss';
+function App() {
+  return (
+  <>
+  <div class="container">
+    <h1>프론트엔드 개발자 김용희입니다.</h1>
+    <div class="gallery-wrap">
+    <div class="item item-1"></div>
+    <div class="item item-2"></div>
+    <div class="item item-3"></div>    
+    </div>
+  </div>
+{/* <div class="social">
+  <a href="https://twitter.com/StefCharle" target="_blank">
+    <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/149103/twitter.svg" alt=""/>
+  </a>
+</div> */}
+  </>
+  );
+}
+
+export default App;
