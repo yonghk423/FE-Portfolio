05529df4c5cd644bea0d438d91cb5e6aa0e4da92
yonghk423 <yonghk423423@gmail.com>
2022-04-10 16:54:29 +0900

    start

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+
 import logo from './logo.svg';
 // import './App.css';
 import './App.scss';
@@ -11,7 +13,7 @@ function App() {
           <li><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
           <li><a href="#specs">Specs</a></li>
-					<li><a href="#contact">Contact</a></li>
+          <li><a href="#contact">Contact</a></li>
         </ul>
       </div>
     </nav>
@@ -39,7 +41,7 @@ function App() {
             <h2 className='titlethr'>우리말을 재밌게 배워 바르게 쓸 수 있도록 관련된 퀴즈를 제공하는 사이트입니다 </h2>
             <h2 className='titlefour'>포지션 : 팀장 프론트엔드</h2>
             <h2 className='titlefive'>기술스텍</h2>  
-            <h2 className='titlesix'> JavaScript, React JS, React Hooks API, Axios, React Router, <br></br> Styled-Components </h2>
+            <h2 className='titlesix'> JavaScript, React JS, React Hooks API, Axios, React Router, Styled-Components </h2>
           </div>
           <img className='zero' src='https://media.vlpt.us/images/yonghk423/post/590c8996-fac0-4740-b28b-2ecf80a5b939/LandingPage.gif'></img>
         </div> 
@@ -51,7 +53,7 @@ function App() {
             <p>MyPage는 기본적으로 회원의 email, name, image, mileage 와 구매내역 내가 만든 문제를 볼 수가 있습니다. 처음 데이터 초기값 설정과 데이터를 어떤 방식으로 받아 와야 할지 고민하였습니다. 구현 방법으로 HTTP 비동기 통신 라이브러리인 Axios와 React Hooks API인 useState와 useEffect를 사용하였습니다. </p>
             <h2>Axios, useState, useEffect 왜 사용하였는가</h2>
             <p>Axios : Fetch API를 사용하지 않고 Axios를 사용한 이유는 자동으로 JSON 데이터 형식으로 변환을 해주고 좀 더 많은 브라우저에 지원되며 실무에서 Fetch API보다 더 선호하는 경향이 있기 때문입니다.</p>
-            <p>useState : 리액트 16.8 이전 버전에서는 함수형 컴포넌트에서는 상태를 관리할 수 없었지만, 리액트 16.8 에서 React Hooks API 기능이 도입되면서 함수형 컴포넌트에서도 상태를 관리할 수 있게 되었습니다. 리액트는 부모 컴포넌트 렌더링, state , props가 변경 될 때 리렌더링이 됩니다. 따라서 데이터 변경시 리렌더링이 되려면 useState를 사용해야하며 계속 일정한 데이터를 기억하도록 해야하고 변수가 변경될 때 자동으로 관련된 리렌더링 되게 만들고 싶으면 변수가 아닌 state에 저장해서 데이터바인딩을 해야 합니다.</p> 
+            <p>useState : 리액트 16.8 이전 버전에서는 함수형 컴포넌트에서는 상태를 관리할 수 없었지만, 리액트 16.8 에서 React Hooks API 기능이 도입되면서 함수형 컴포넌트에서도 상태를 관리할 수 있게 되었습니다. 리액트는 부모 컴포넌트 렌더링, state , props가 변경 될 때 리렌더링이 됩니다. 따라서 데이터 변경시 리렌더링이 되려면 useState를 사용해야하며 계속 일정한 데이터를 기억하도록 해야하고 상태가 변경될 때 자동으로 관련된 리렌더링이 되게 만들고 싶으면 변수가 아닌 state에 저장해서 데이터바인딩을 해야 합니다.</p> 
             <p>useEffect : useEffect는 렌더링 직후 추가 작업을 설정할 수 있는 React Hooks API입니다 따라서 렌더링 직후 useEffect를 이용한 Axios 비동기 통신을 호출하고 받아온 데이터 값을 useState를 통하여 상태 관리를 하도록 구성하였습니다.</p>
           </div>
           <div className='imgone'> 
@@ -63,15 +65,28 @@ function App() {
               <h2>MyPage RankModal</h2>
               <h2>모달창 UI</h2>
               <p>position: fixed을 사용하여 특정 위치에 고정시켜 스크롤을 내려도 그 자리에 계속 위치하게 설정 시키며 z-index을 이용하여 박스를 화면 우선 순위로 지정하였다. z-index 속성이 적용되기 위해서는 z-index를 적용한 영역이 position:static 값이 아니어야 한다는 점을 주의하였다.</p>
-              <h2>onClick 이벤트 발생시 모달 상자 띄우기, 데이터 출력(Pagination)</h2>
-              <p>모달창을 만드는 핵심은 웹 브라우저 페이지를 하나의 큰창으로 잡고 그 안에 박스 하나를 만들어 출력하는 방식이다 처음에는 새로운 창을 띄우는 것이 아닌데 착각하여 라우터 이용하고자 하였다. Pagination : 모달창 이 열리게 되면 api 요청을 통해 데이터를 출력해야 하는데 목록형 UI(상품 등)에서 데이터를 한번에 가져오는것이 아니라 필요한만큼, 또는 화면에 보이는 만큼만 백엔드에    요청하고 응답받아 사용하는 방식을 활용하여 데이터를 받아 왔다. 
-              필요한 만큼 받아 오려면 구간을 정해줘야 하는데 limit값과 offset값을 설정하였으며 또한 버튼을 클릭 하였을 때 추가적으로 받아 올 수 있도록 설정하였다.</p>
+              <h2>useState를 이용한 onClick 이벤트 발생시 모달 상자 띄우기</h2>
+              <p>모달창을 띄우려면 버튼 클릭시 모달 상자를 띄우고 다시 버튼 클릭시 상자를 닫는 과정이 기본적인 로직의 순서입니다. 앞에서 말했던 것 처럼 상태가 변경 될 때 자동으로 관련된 리렌더링이 되게 하려면 useState를 사용해야 해아 합니다. 처음 상태값을 Boolean 타입인 false로 설정하고 onClick 이벤트 발생시 상태값을 true로 변경하여 상자를 띄우고 반대로 상자를 닫을 때는 true였던 값을 다시 false로 상태를 변경하도록 하였습니다.   
+              <h2>useState, useEffect를 이용한 데이터 출력(limit, offset 값 설정)</h2>  
+                 모달창이 열리게 되면 api 요청을 통해 데이터를 출력해야 하는데 목록형 UI(상품 등)에서 데이터를 한번에 가져오는것이 아니라 필요한만큼, 또는 화면에 보이는 만큼만 백엔드에 요청하고 응답받아 사용하는 방식을 활용하여 데이터를 받아 오는 형식이 필요했습니다. 필요한 만큼 받아 오려면 구간을 정해줘야 하는데 limit값과 offset값을 설정하고 버튼을 클릭 하였을 때 추가적으로 받아 올 수 있도록 useEffect를 사용하여 limit값이 변경시에만 useEffect가 실행 되도록하여 버튼을 클릭시 추가적인 데이터가 나오도록 구성하였습니다.</p>
             </div>
             <img className='two' src='https://media.vlpt.us/images/yonghk423/post/ef581d6b-fd99-4dd0-8587-9a6c97281e38/마이페이지-rankmodal.gif'></img>       
+          </div>
+          <div className='gridfive'>
+            <div className='textBox'>
+              <h2>로그인 회원가입 UI</h2>
+              <p></p>
+            </div>
+            <div className='imgBox'>
+              <img className='loginImg' src='https://media.vlpt.us/images/yonghk423/post/6a90c5e4-e2d9-4241-9ca7-4e43d99d520e/Login.gif' alt=''></img>
+              <img className='signUpImg' src='https://media.vlpt.us/images/yonghk423/post/525a7bc0-f56f-4740-a026-412046315040/SignUp.gif' alt=''></img>
+            </div>
           </div>                  
         </div>
       </div>
     </section>   
+
+    <section class="section-a">test</section>
     
     <footer class="section-footer py-4 bg-primary">
       <div class="container">
@@ -134,7 +149,10 @@ function App() {
       </div>
     </footer>  
   </>
-  );
-}
+  )
+  
+  };
+
 
 export default App;
+
